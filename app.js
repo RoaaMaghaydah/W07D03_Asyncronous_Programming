9a9cfbdb2a574d095226cb8d55300cfc34c30b17
RoaaMaghaydah <roaa.maghaydah@gmail.com>
2021-05-11 13:57:28 +0300

    practice q1 & q2

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const fs = require("fs");
 const axios = require("axios");
 
+const readFile = () => {
 fs.readFile("data.txt", (err, data) => {
     if (err) throw err;
     content = data.toString();
     console.log(content);
   });
-
-  
+}
+  readFile();
+  const writeFile = () => {
 fs.writeFile("text.txt", "A new file has been created", (err) => {
     if (err) throw err;
     console.log("done");
   });
+}
 
+  writeFile();
   const getPost = (id) => {
     axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
     .then((response) => {
@@ -24,14 +28,42 @@ fs.writeFile("text.txt", "A new file has been created", (err) => {
       });
     };
   
-  //getPost(1);
- // getPost(50);
+  getPost(1);
+  getPost(50);
 
   const getPostAsync =async (id) => {
     const response= await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
     console.log("111",response.data)
   };
-  getPostAsync(1);
-  getPostAsync(50);
+  //getPostAsync(1);
+  //getPostAsync(50);
+
   
+  const appendToFile = (data) => {
+    fs.appendFile('data.txt', data, (err) => {
+        if (err) throw err;
+        console.log('The "data to append" was appended to file!');
+      });
+    };
+// appendToFile("append done  ");
+
+const copyFile = (fileName) => {
+    fs.copyFile('data.txt', 'copy_of_data.txt', (err) => {
+        if (err) throw err;
+        console.log('source.txt was copied to destination.txt');
+      });
+  };
+
+  copyFile();
+
+  const post = JSON.stringify({
+    title: "JavaScript Basics",
+    body: "This post contains information about javaScript ",
+    // the id of the user who is going to create the post
+    userId: 1,
+  });
+
+  const createPost = (user) => {
+   
+  };
   
