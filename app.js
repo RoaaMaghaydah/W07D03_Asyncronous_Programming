c43a819d54029469df667b6e831df5c7d7704805
RoaaMaghaydah <roaa.maghaydah@gmail.com>
2021-05-11 13:18:30 +0300

    pulse cheak

--- app.js
+++ app.js
@@ -13,4 +13,25 @@ fs.writeFile("text.txt", "A new file has been created", (err) => {
     console.log("done");
   });
 
+  const getPost = (id) => {
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    .then((response) => {
+        console.log('DATA:', response.data);
+    
+      })     
+      .catch((error) => {
+        console.log('ERR:', error);
+      });
+    };
+  
+  //getPost(1);
+ // getPost(50);
+
+  const getPostAsync =async (id) => {
+    const response= await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    console.log("111",response.data)
+  };
+  getPostAsync(1);
+  getPostAsync(50);
+  
   
